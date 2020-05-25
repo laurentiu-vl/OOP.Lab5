86d6842a781e3ca38f6b33daebda0b7fa608dfa0
phayzee6002 <56519529+phayzee6002@users.noreply.github.com>
2020-05-25 15:14:18 +0300

    fix html

--- Cinema/HTML.cpp
+++ Cinema/HTML.cpp
@@ -9,16 +9,14 @@ HTML::HTML()
 
 string HTML::write_watchlist_in_file(vector<Film> v)
 {
-	auto filename = string("C:\\temp\\watchlist.html");
-	ofstream html(filename, ios::out || ios::trunc);
-	
-	//html.open(filename, ofstream::out || ofstream::trunc);
+	ofstream myfile;
+	myfile.open("watchlist.html");
 
-	html << "<!DOCTYPE html>\n<html>\n<head>\n<title>Watchliste</title>\n</head>\n<body>\n<table style='align='center''>\n<tr style='font-weight:bold'>\n<td>Title</td>\n<td>Genre</td>\n<td>Releaseyear</td>\n<td>Likes</td>\n<td>Trailer link</td>\n</tr>\n";
+	myfile << "<!DOCTYPE html>\n<html>\n<head>\n<title>Watchliste</title>\n</head>\n<body>\n<table style='align='center''>\n<tr style='font-weight:bold'>\n<td>Title</td>\n<td>Genre</td>\n<td>Releaseyear</td>\n<td>Likes</td>\n<td>Trailer link</td>\n</tr>\n";
 	for (int i = 0; i < v.size(); i++)
-		html << "<tr>\n<td>" << v[i].get_titel() << "</td>\n<td>" << v[i].get_genre() << "</td>\n<td>" << v[i].get_jahr() << "</td>\n<td>" << v[i].get_anz_likes() << "</td>\n<td><a href=" << v[i].get_trailer() << ">Link</a>\n</td>\n</tr>\n";
-	html << "</table>\n</body>\n</html>\n";
-	html.flush();
-	html.close();
-	return filename;
+		myfile << "<tr>\n<td>" << v[i].get_titel() << "</td>\n<td>" << v[i].get_genre() << "</td>\n<td>" << v[i].get_jahr() << "</td>\n<td>" << v[i].get_anz_likes() << "</td>\n<td><a href=" << v[i].get_trailer() << ">Link</a>\n</td>\n</tr>\n";
+	myfile << "</table>\n</body>\n</html>\n";
+	myfile.flush();
+	myfile.close();
+	return "watchlist.html";
 }
